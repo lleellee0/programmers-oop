ca357a601ebcbb0da567bc5d8516c5374d3d133d
lleellee0 <lleellee013@gmail.com>
2022-10-10 07:05:27 +0900

    추상 메서드 호출하도록 변경

--- src/main/java/kr/co/programmersoop/example/section_01_04/abstract_class/AbstractClass.java
+++ src/main/java/kr/co/programmersoop/example/section_01_04/abstract_class/AbstractClass.java
@@ -3,6 +3,7 @@ package kr.co.programmersoop.example.section_01_04.abstract_class;
 public abstract class AbstractClass {
     public void implementedMethod() {
         System.out.println("AbstractClass implementedMethod");
+        this.abstractMethod();
     }
 
     abstract public void abstractMethod();
