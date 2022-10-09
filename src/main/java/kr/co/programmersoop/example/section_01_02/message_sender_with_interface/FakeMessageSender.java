90bca6a42b9bbc83dcc1721af83583c2d036a872
lleellee0 <lleellee013@gmail.com>
2022-10-09 22:02:09 +0900

    띄어쓰기 누락 수정

--- src/main/java/kr/co/programmersoop/example/section_01_02/message_sender_with_interface/FakeMessageSender.java
+++ src/main/java/kr/co/programmersoop/example/section_01_02/message_sender_with_interface/FakeMessageSender.java
@@ -1,6 +1,6 @@
 package kr.co.programmersoop.example.section_01_02.message_sender_with_interface;
 
-public class FakeMessageSender implements MessageSender{
+public class FakeMessageSender implements MessageSender {
     public void send() {
         // 메시지는 안보내고 메시지를 보냈다는 로그만 찍기
     }
