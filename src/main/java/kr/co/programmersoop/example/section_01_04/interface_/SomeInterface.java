d9fc01bdcc5a85f36d9b3bb008cc981fb24e98fc
lleellee0 <lleellee013@gmail.com>
2022-10-10 17:46:24 +0900

    인터페이스에 default 메서드 추가

--- src/main/java/kr/co/programmersoop/example/section_01_04/interface_/SomeInterface.java
+++ src/main/java/kr/co/programmersoop/example/section_01_04/interface_/SomeInterface.java
@@ -2,4 +2,9 @@ package kr.co.programmersoop.example.section_01_04.interface_;
 
 public interface SomeInterface {
     void someMethod();
+
+    default void defaultMethod() {
+//        인터페이스에 메서드 정의 가능
+        this.someMethod();
+    }
 }
