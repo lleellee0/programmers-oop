5a763f353b7e110514801d82af29ff88675a5d59
lleellee0 <lleellee013@gmail.com>
2022-10-21 23:52:07 +0900

    오탈자 수정, sameObject들에 대한 hashCode 메서드 결과 출력 추가

--- src/main/java/kr/co/programmersoop/example/section_01_07/hash_code/HashCodeExampleMain.java
+++ src/main/java/kr/co/programmersoop/example/section_01_07/hash_code/HashCodeExampleMain.java
@@ -6,10 +6,13 @@ import java.util.Set;
 public class HashCodeExampleMain {
     public static void main(String[] args) {
         // Hash 관련 컬렉션이 같은 인스턴스를 구분하는 방법
-        // equals 비교 -> hashCode 비교
+        // hashCode 비교 -> equals 비교
         
         SomeObject sameObject1 = new SomeObject(1, "programmers");
         SomeObject sameObject2 = new SomeObject(1, "programmers");
+
+        System.out.println(sameObject1.hashCode());
+        System.out.println(sameObject2.hashCode());
 
         Set<SomeObject> set = new HashSet<>();
 
